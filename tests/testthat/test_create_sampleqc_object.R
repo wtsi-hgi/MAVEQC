ac4220c3a1067af9bde0b6c8546172a3197ede3b
sk34 <sk34@sanger.ac.uk>
2025-03-19 14:35:00 +0000

    Add descriptive variable names in unit test

--- tests/testthat/test_create_sampleqc_object.R
+++ tests/testthat/test_create_sampleqc_object.R
@@ -6,18 +6,17 @@ test_that("Test create_sampleqc_object works correctly", {
     # create a relative path to the plasmid data
     plasmid_test_data_path <- test_path("testdata", "plasmid")
 
-    # mock the import_sge_files function with mocked plasmid data
-    mocked_sge_objs <- import_sge_files(plasmid_test_data_path, "sample_sheet.tsv")
+    # import_sge_files function with path to plasmid data sample sheet
+    sge_objs <- import_sge_files(plasmid_test_data_path, "sample_sheet.tsv")
 
-    # mock the create_sampleqc_object function
-    mocked_sampleqc_object <- create_sampleqc_object(mocked_sge_objs)
+    sampleqc_object <- create_sampleqc_object(sge_objs)
 
-    # test the mocked sampleqc object
-    expect_s4_class(mocked_sampleqc_object, "sampleQC")
-    expect_equal(length(mocked_sampleqc_object@samples), 1)
-    expect_equal(mocked_sampleqc_object@samples_meta$sample_name, c("testsample_hdr741"))
-    expect_equal(ncol(mocked_sampleqc_object@stats), 28)
-    expect_equal(rownames(mocked_sampleqc_object@stats), c("testsample_hdr741"))
-    expect_equal(mocked_sampleqc_object@counts[[1]]$sequence, "ATGTTACAGGTTTAGTTTCTGTCGGTCGCCTCAAGGGTGTAGTCTT")
-    expect_equal(mocked_sampleqc_object@counts[[1]]$count, 1)
+    # test expected sample vaules in sampleqc object
+    expect_s4_class(sampleqc_object, "sampleQC")
+    expect_equal(length(sampleqc_object@samples), 1)
+    expect_equal(sampleqc_object@samples_meta$sample_name, c("testsample_hdr741"))
+    expect_equal(ncol(sampleqc_object@stats), 28)
+    expect_equal(rownames(sampleqc_object@stats), c("testsample_hdr741"))
+    expect_equal(sampleqc_object@counts[[1]]$sequence, "ATGTTACAGGTTTAGTTTCTGTCGGTCGCCTCAAGGGTGTAGTCTT")
+    expect_equal(sampleqc_object@counts[[1]]$count, 1)
 })
