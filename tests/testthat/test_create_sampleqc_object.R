89dcf40f2c9cae5ce1ec46d0aa45112aec958c52
sk34 <sk34@sanger.ac.uk>
2025-03-20 14:51:36 +0000

    Add more test cases for sampleqc object

--- tests/testthat/test_create_sampleqc_object.R
+++ tests/testthat/test_create_sampleqc_object.R
@@ -13,10 +13,18 @@ test_that("Test create_sampleqc_object works correctly", {
 
     # test expected sample vaules in sampleqc object
     expect_s4_class(sampleqc_object, "sampleQC")
-    expect_equal(length(sampleqc_object@samples), 1)
-    expect_equal(sampleqc_object@samples_meta$sample_name, c("testsample_hdr741"))
+
+    expect_equal(class(sampleqc_object@samples), "list")
+    expect_equal(sampleqc_object@lengths[[2]]$length, c(236, 150))
+    expect_equal(length(sampleqc_object@samples), 2)
+    expect_equal(sampleqc_object@samples_meta$sample_name, c("test_hdr741", "test_hdr742"))
     expect_equal(ncol(sampleqc_object@stats), 28)
-    expect_equal(rownames(sampleqc_object@stats), c("testsample_hdr741"))
-    expect_equal(sampleqc_object@counts[[1]]$sequence, "ATGTTACAGGTTTAGTTTCTGTCGGTCGCCTCAAGGGTGTAGTCTT")
-    expect_equal(sampleqc_object@counts[[1]]$count, 1)
+    expect_equal(sampleqc_object@counts[[1]]$sequence[1], "GAAGAAGAACTGTGACTCATCCTGAAAACCTCTTTGAGGATTGATAGCATTTCATCAGAATCGCCATTTTGATCTACGGTTTTTGATTGCTTAGATTTTGGCAATTTTTTAGGATTAGGATTATCTATAGCACTGTCAGAAGATTTACGCTTATCCTTTTTTCTCACTGGAACTTATAGTTTTTGTTTCTCCTTAACTGTTTCATTACATTCTTCATCTGAATTAGATGTTACAGGTTTAGTTTCTGTCGGTCGCCTCAAGGGTGTAGTCTT")
+    expect_equal(length(sampleqc_object@lengths), 2)
+    expect_equal(length(sampleqc_object@samples_ref), 2)
+
+    expect_equal(class(sampleqc_object@samples_ref), "list")
+    expect_equal(sampleqc_object@samples_ref[[1]]@libcounts$length, c(272, 272))
+    expect_equal(sampleqc_object@samples_ref[[1]]@libcounts$count, c(1546465, 4145))
+    
 })
