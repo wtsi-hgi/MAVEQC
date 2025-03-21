5900862a3b670272c77e995c1b8c1d3468e9c1b7
sk34 <sk34@sanger.ac.uk>
2025-03-21 16:45:48 +0000

    Add descriptive test title and remove actual path to luster in test data files

--- tests/testthat/test_classes.R
+++ tests/testthat/test_classes.R
@@ -1,7 +1,7 @@
 # Import unit test library testthat
 library(testthat)
 
-test_that("create_sampleqc_object returns an object of class 'sampleQC'", {
+test_that("create_sampleqc_object returns expected object of class 'sampleQC'", {
    
     # create a relative path to the plasmid data
     plasmid_test_data_path <- test_path("testdata", "plasmid")
